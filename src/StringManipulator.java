195fb857acf4672e07696720d4f0e52bf39ac938
Casey <Casey@192.168.1.205>
2018-10-25 21:33:24 -0400

    Completed assignment

--- src/StringManipulator.java
+++ src/StringManipulator.java
@@ -22,6 +22,19 @@ public class StringManipulator
 		System.out.println("Length of string: " + strLength);
 		System.out.println("First half of string: " + str.substring(0, strLength/2));
 		
+		System.out.print("\nPlease enter a character to search for: ");
+		String letter = user.nextLine();
+		System.out.println("Index of character: " + str.indexOf(letter));
+		System.out.print("\nPlease give a start index for a substring: ");
+		String start = user.nextLine();
+		int startIndex = Integer.parseInt(start);
+		System.out.print("\nPlease give a stop index for a substring: ");
+		String stop = user.nextLine();
+		int stopIndex = Integer.parseInt(stop);
+		System.out.print("\nSubstring: " + str.substring(startIndex, stopIndex));
+		
+		
+		
 		
 
 	}
